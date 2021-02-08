2c109bb683b10597290cebbdd0c11f5c7059075b
Andy Zhou <zhoua@sunypoly.edu>
2021-02-08 12:17:49 -0500

    commit message

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed;
+    public Rigidbody2D rb;
+
+    public float jumpForce = 10f;
+    public Transform feet;
+    public LayerMask groundLayers;
+
+    float mx;
+
+    // Update is called once per frame
+    void Update()
+    {
+        mx = Input.GetAxisRaw("Horizontal");
+        
+        if (Input.GetButtonDown("Jump") && isGrounded()){
+            Jump();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //Vector2 movement = new Vector2(mx * movementSpeed, rb.velocity.y);
+
+        //rb.velocity = movement;
+    }
+
+    void Jump()
+    {
+        //Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
+        if(mx != 0){
+            Vector2 movement = new Vector2(mx * movementSpeed, jumpForce);
+
+            rb.velocity = movement;
+        }
+    }
+
+    public bool isGrounded()
+    {
+        Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 0.5f, groundLayers);
+
+        return groundCheck;
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# CS370GroupDJumpKing
+Goal: Creating a 2D platformer like the ~~rage inducing~~ friendly platformer game Jump King.
+
+Step 1: Learn Unity
