8a9fa669d3a140652299e0d006f0415ed8193854
Andy Zhou <zhoua@sunypoly.edu>
2021-02-08 15:52:09 -0500

    Nothing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -34,7 +34,6 @@ public class PlayerMovement : MonoBehaviour
             moveTime *= sign;
             Jump();
         }
-
         
         /*
         if (Input.GetButtonDown("Jump") && isGrounded()){
