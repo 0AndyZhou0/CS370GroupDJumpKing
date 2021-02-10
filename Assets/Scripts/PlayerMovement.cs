70f7dceb0c26eff146483d617b32d78aaffd44a1
Andy Zhou <zhoua@sunypoly.edu>
2021-02-10 14:30:29 -0500

    revert to old

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,26 +6,46 @@ public class PlayerMovement : MonoBehaviour
 {
     public float movementSpeed;
     public Rigidbody2D rb;
+    public float MAX_SPEED = 0.5f;
 
-    public float jumpForce = 10f;
+    public float jumpForce = 20f;
     public Transform feet;
     public LayerMask groundLayers;
 
-    float mx;
+    float startTime;
+    float sign;
+    float moveTime;
 
     // Update is called once per frame
     void Update()
     {
-        mx = Input.GetAxisRaw("Horizontal");
-        
-        if (Input.GetButtonDown("Jump") && isGrounded()){
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            startTime = Time.time;
+            sign = Input.GetAxisRaw("Horizontal");
+        }
+
+        if (Input.GetButtonUp("Horizontal") && isGrounded())
+        {
+            moveTime = Time.time - startTime;
+            if(moveTime > MAX_SPEED){
+                moveTime = MAX_SPEED;
+            }
+            moveTime *= sign;
             Jump();
         }
+        
+        /*
+        if (Input.GetButtonDown("Jump") && isGrounded()){
+            Jump();
+            startTime = 0;
+        }
+        */
     }
 
     void FixedUpdate()
     {
-        //Vector2 movement = new Vector2(mx * movementSpeed, rb.velocity.y);
+        //Vector2 movement = new Vector2(moveTime * movementSpeed, rb.velocity.y);
 
         //rb.velocity = movement;
     }
@@ -33,8 +53,8 @@ public class PlayerMovement : MonoBehaviour
     void Jump()
     {
         //Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
-        if(mx != 0){
-            Vector2 movement = new Vector2(mx * movementSpeed, jumpForce);
+        if(moveTime != 0){
+            Vector2 movement = new Vector2(moveTime * movementSpeed, Mathf.Abs(moveTime * jumpForce));
 
             rb.velocity = movement;
         }
