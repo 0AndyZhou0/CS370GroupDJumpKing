3415478522e17e982d86e9b723e4cf4dc2d6d640
Caleb-DeWire <78701539+Caleb-DeWire@users.noreply.github.com>
2021-02-24 14:13:04 -0500

    Basic Main Menu/ Include TextMesh Pro

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+    public void lvlSelect()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void settings()
+    {
+
+    }
+    
+}
