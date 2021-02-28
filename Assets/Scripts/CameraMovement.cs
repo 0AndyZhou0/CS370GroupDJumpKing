b8612951daeaae75459337ce9cfdd85da8aceeba
Andy Zhou <zhoua@sunypoly.edu>
2021-02-27 20:07:07 -0500

    Camera locks on and follows player vertically

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+    public GameObject player;
+
+    void Start()
+    {
+        Vector3 setPosition = transform.position;
+        setPosition.x = player.transform.position.x;
+        setPosition.y = player.transform.position.y;
+        transform.position = setPosition;
+    }
     // Update is called once per frame
     void Update()
     {
-        //Vector3 setPosition = transform.position;
-        //setPosition.x = -14.0f;
-        //setPosition.y = transform.position.y;
-        //transform.position = setPosition;
+        Vector3 setPosition = transform.position;
+        //Debug.Log(setPosition);
+        //Debug.Log(player.transform.position);
+        setPosition.y = player.transform.position.y;
+        transform.position = setPosition;
     }
 }
