2e7de1f414d9525167f634a555932c48b1d313ad
Caleb-DeWire <78701539+Caleb-DeWire@users.noreply.github.com>
2021-03-14 15:32:17 -0400

    Fixed movement, updated controller and minor animation tweaks

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,7 +10,11 @@ public class PlayerMovement : MonoBehaviour
     float MIN_SPEED = 0.4f;
     float MAX_SPEED = 1.2f;
     float GAME_SPEED = 1.7f;
+
     public Animator anim;
+    int skinSelect = 0;
+    bool firstThrough = true;
+
     //[SerializeField] private Transform groundCheckTransform = null;
     [SerializeField] private LayerMask groundLayerMask;
 
@@ -37,24 +41,24 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(chargingJump){
+        if (chargingJump) {
             barBackground.transform.localScale = new Vector3(0.1f, 1.0f, 1.0f);
             chargeLevel = Time.time - startTime;
-            if(chargeLevel > MAX_SPEED - MIN_SPEED){
+            if (chargeLevel > MAX_SPEED - MIN_SPEED) {
                 bar.transform.localScale = new Vector3(0.1f, 1.0f, 1.0f);
                 bar.transform.localPosition = new Vector3(-1.93f, -2.5f, 0.0f);
             }
-            else if(chargeLevel > 0.0f){
+            else if (chargeLevel > 0.0f) {
                 chargeLevel /= 0.8f;
                 bar.transform.localScale = new Vector3(0.1f, chargeLevel, 1.0f);
-                bar.transform.localPosition = new Vector3(-1.93f, -3.0f+(chargeLevel/2), 0.0f);
+                bar.transform.localPosition = new Vector3(-1.93f, -3.0f + (chargeLevel / 2), 0.0f);
             }
-        }else{
+        } else {
             bar.transform.localScale = new Vector3(0.1f, 0.0f, 1.0f);
             barBackground.transform.localScale = new Vector3(0.1f, 0.0f, 1.0f);
         }
 
-        if(IsGrounded() && !chargingJump)
+        if (IsGrounded() && !chargingJump)
         {
             direction = Input.GetAxisRaw("Horizontal");
             /*
@@ -64,7 +68,7 @@ public class PlayerMovement : MonoBehaviour
             }
             */
         }
-        
+
         if (IsStill() && IsGrounded() && Input.GetButtonDown("Jump") && !chargingJump)
         {
             chargingJump = true;
@@ -75,7 +79,7 @@ public class PlayerMovement : MonoBehaviour
         {
             chargeTime = Time.time - startTime;
             chargeTime += MIN_SPEED;
-            if(chargeTime > MAX_SPEED){
+            if (chargeTime > MAX_SPEED) {
                 chargeTime = MAX_SPEED;
             }
             direction = Input.GetAxisRaw("Horizontal");
@@ -101,55 +105,70 @@ public class PlayerMovement : MonoBehaviour
         }
         anim.SetBool("isGrounded", IsGrounded());
 
+        if (firstThrough) {
+            switch (skinSelect)
+            {
+                case 0:
+                    anim.SetBool("GreyGuySelect", true);
+                    anim.SetBool("GhostSelect", false);
+                    break;
+                case 1:
+                    anim.SetBool("GreyGuySelect", false);
+                    anim.SetBool("GhostSelect", true);
+                    break;
+            }
+            firstThrough = false;
+        }
+
         //Debug.Log(IsGrounded());
         //Debug.Log(notJumping);
     }
 
     void FixedUpdate()
     {
-        
-        if(readyToJump && IsStill()){
+
+        if (readyToJump && IsStill()) {
             Jump();
         }
 
-        if(Approximately(rb.velocity.y, 0.0f, 0.01f)){
+        if (Approximately(rb.velocity.y, 0.0f, 0.01f)) {
             notJumping = true;
-        }else{
+        } else {
             notJumping = false;
         }
 
-        if(IsGrounded() && !chargingJump){
+        if (IsGrounded() && !chargingJump) {
             Vector2 movement = new Vector2(direction * movementSpeed, 0);
             rb.velocity = movement;
         }
     }
 
-    bool Approximately(float a, float b, float e){
+    bool Approximately(float a, float b, float e) {
         //Debug.Log(Mathf.Abs(a - b));
         return Mathf.Abs(a - b) < e;
-    } 
-    
+    }
+
     void OnCollisionEnter2D(Collision2D col)
     {
         //Debug.Log(IsGrounded());
-        if(!IsGrounded()){
+        if (!IsGrounded()) {
             Vector2 normal = col.GetContact(0).normal;
             //Debug.Log(normal);
 
             //flat ground
-            if(Approximately(normal.x, 0.0f, 0.01f) && Approximately(normal.y, 1.0f, 0.01f)){
+            if (Approximately(normal.x, 0.0f, 0.01f) && Approximately(normal.y, 1.0f, 0.01f)) {
                 notJumping = true;
             }
             //flat roof
-            else if(Approximately(normal.x, 0.0f, 0.01f) && Approximately(normal.y, -1.0f, 0.01f)){}
-            else{
+            else if (Approximately(normal.x, 0.0f, 0.01f) && Approximately(normal.y, -1.0f, 0.01f)) { }
+            else {
                 Vector2 velocity = col.relativeVelocity;
                 //Debug.Log(velocity);
                 Vector2 newVelocity;
                 float dotProduct = velocity.x * normal.x + velocity.y * normal.y;
 
-                newVelocity.x = 2*(dotProduct)*normal.x - velocity.x;
-                newVelocity.y = 2*(dotProduct)*normal.y - velocity.y;
+                newVelocity.x = 2 * (dotProduct) * normal.x - velocity.x;
+                newVelocity.y = 2 * (dotProduct) * normal.y - velocity.y;
 
                 rb.velocity = newVelocity;
             }
@@ -163,7 +182,7 @@ public class PlayerMovement : MonoBehaviour
     void Jump()
     {
         //Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
-        if(chargeTime > 0){
+        if (chargeTime > 0) {
             Vector2 movement = new Vector2(direction * 4.0f, chargeTime * jumpForce);
             rb.velocity = movement;
         }
@@ -186,5 +205,15 @@ public class PlayerMovement : MonoBehaviour
     public bool IsStill()
     {
         return Approximately(rb.velocity.x, 0.0f, 0.01f);
+    }
+
+    public void GreyGuySelected()
+    {
+        skinSelect = 0;
+    }
+
+    public void GhostSelected()
+    {
+        skinSelect = 1;
     }
 }
