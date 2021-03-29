e24e50c16abf4e84948c51d99d30da4862b1056d
Andy Zhou <zhoua@sunypoly.edu>
2021-03-29 15:31:02 -0400

    timer added

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    float timer = 0.0f;
+
+    void Start()
+    {
+        if(PlayerPrefs.HasKey("timer")){
+            timer = PlayerPrefs.GetFloat("timer");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += (Time.deltaTime / 1.7f);
+        PlayerPrefs.SetFloat("timer", timer);
+        //Debug.Log(timer);
+
+        string minutes = ((int) timer / 60).ToString();
+        string seconds = (timer % 60).ToString("f2");
+
+
+        GetComponent<TMPro.TextMeshProUGUI>().text = minutes + seconds;
+    }
+}
