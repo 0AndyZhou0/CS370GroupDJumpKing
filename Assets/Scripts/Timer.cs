d5555ce8e3272144f80353e1d052cf2401af847c
Andy Zhou <zhoua@sunypoly.edu>
2021-04-04 19:17:24 -0400

    timer looks way better now

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -20,10 +20,39 @@ public class Timer : MonoBehaviour
         PlayerPrefs.SetFloat("timer", timer);
         //Debug.Log(timer);
 
-        string minutes = ((int) timer / 60).ToString();
-        string seconds = (timer % 60).ToString("f2");
+        double seconds = timer;
+        int minutes = (int) (seconds / 60.0);
+        int hours = (int) (minutes / 60.0);
+        
+        seconds %= 60.0;
+        minutes %= 60;
 
+        string time = "";
 
-        GetComponent<TMPro.TextMeshProUGUI>().text = minutes + seconds;
+        if(hours == 0){
+            time += "00:";
+        }else if(hours < 10){
+            time += "0" + hours.ToString() + ":";
+        }else{
+            time += hours.ToString() + ":";
+        }
+
+        if(minutes == 0){
+            time += "00:";
+        }else if(minutes < 10){
+            time += "0" + minutes.ToString() + ":";
+        }else{
+            time += minutes.ToString() + ":";
+        }
+        
+        if(seconds == 0){
+            time += "00.00";
+        }else if(seconds < 10){
+            time += "0" + seconds.ToString("f2");
+        }else{
+            time += seconds.ToString("f2");
+        }
+
+        GetComponent<TMPro.TextMeshProUGUI>().text = time;
     }
 }
