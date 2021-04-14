dc2d31bfaf3026c0c12c29f96227cba855be3b60
Andy Zhou <zhoua@sunypoly.edu>
2021-04-14 14:59:48 -0400

    started working on collapsing script

---
+++ Assets/CollapsingTile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class CollapsingTile : MonoBehaviour
+{
+    bool isIntact = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col){
+        Tilemap collapsingTileset = this.GetComponent<Tilemap>();
+        foreach (ContactPoint2D tile in col.contacts)
+        {
+            Vector2 tilePosition = new Vector2();
+            tilePosition.x = tile.point.x - 0.01f * tile.normal.x;
+            tilePosition.y = tile.point.y - 0.01f * tile.normal.y;
+            collapsingTileset.SetTile(collapsingTileset.WorldToCell(tilePosition), null);
+        }
+    }
+}
