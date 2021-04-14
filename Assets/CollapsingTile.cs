f3f84097725635856fb94945a2396c89e19cd99a
Andy Zhou <zhoua@sunypoly.edu>
2021-04-14 15:12:25 -0400

    collapsing ground script

--- Assets/CollapsingTile.cs
+++ Assets/CollapsingTile.cs
@@ -13,14 +13,24 @@ public class CollapsingTile : MonoBehaviour
         
     }
 
+    
+    //Math
+
+    bool Approximately(float a, float b, float e) {
+        //Debug.Log(Mathf.Abs(a - b));
+        return Mathf.Abs(a - b) < e;
+    }
+
     void OnCollisionEnter2D(Collision2D col){
         Tilemap collapsingTileset = this.GetComponent<Tilemap>();
         foreach (ContactPoint2D tile in col.contacts)
         {
-            Vector2 tilePosition = new Vector2();
-            tilePosition.x = tile.point.x - 0.01f * tile.normal.x;
-            tilePosition.y = tile.point.y - 0.01f * tile.normal.y;
-            collapsingTileset.SetTile(collapsingTileset.WorldToCell(tilePosition), null);
+            if(Approximately(tile.normal.x, 0.0f, 0.01f) && Approximately(tile.normal.y, -1.0f, 0.01f)){
+                Vector2 tilePosition = new Vector2();
+                tilePosition.x = tile.point.x + 0.01f * tile.normal.x;
+                tilePosition.y = tile.point.y + 0.01f * tile.normal.y;
+                collapsingTileset.SetTile(collapsingTileset.WorldToCell(tilePosition), null);
+            }
         }
     }
 }
